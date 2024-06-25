03d615e20367e9d0f3837c83831326bc65882e73
Liliana Padilla <liliana.elizabeth.pama@gmail.com>
2024-06-25 14:14:50 -0600

    Nuevos cambios

--- lili.js
+++ lili.js
@@ -3,36 +3,34 @@
 // var resultado = 0;
 
 var mensaje = "Hola",
-    precio = 14.56,
-    resultado = 0;
+  precio = 14.56,
+  resultado = 0;
 
 function hola() {
-    console.log(mensaje);
-    console.log(resultado);
-    console.log(precio)
+  console.log(mensaje);
+  console.log(resultado);
+  console.log(precio);
 }
 hola();
-
-
 
 //arreglos y objetos
 var liliArreglo = ["lili", 28, 1.54];
 liliArreglo.push(123);
 var lili = {
-    name: "lili",
-    age: 28
+  name: "lili",
+  age: 28,
 };
 lili.estatura = 355.5;
 lili.age = 29;
-function imprimirPersonaObj(){
-
-    console.log(lili);
+function imprimirPersonaObj() {
+  console.log(lili);
 }
 imprimirPersonaObj();
 
 function imprimirPersonaArray() {
-    console.log(liliArreglo);
+  console.log(liliArreglo);
 }
+
 imprimirPersonaArray();
 
-
+//hola
